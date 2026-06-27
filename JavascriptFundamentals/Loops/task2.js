246f3e88b0ebcb58aec6f65127e5ec929eb24e26
alish-gautam <alishgautam890@gmail.com>
2026-06-27 09:35:04 +0545

    For loop done

---
+++ JavascriptFundamentals/Loops/task2.js
@@ -0,0 +1,9 @@
+// Output even numbers in the loop
+// importance: 5
+// Use the for loop to output even numbers from 2 to 10.
+
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
