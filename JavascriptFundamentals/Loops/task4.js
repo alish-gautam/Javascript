4f3d7643269a521796feca89a402c0d9ed544c4a
alish-gautam <alishgautam890@gmail.com>
2026-06-27 09:55:20 +0545

    task done

---
+++ JavascriptFundamentals/Loops/task4.js
@@ -0,0 +1,15 @@
+// Repeat until the input is correct
+// importance: 5
+// Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+let number = prompt("Enter a number please");
+while (number < 100 && number != null) {
+  number = prompt("Enter a number please");
+  console.log(number);
+}
+
+console.log(number);
