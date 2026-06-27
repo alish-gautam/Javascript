4cdefa9c07e505fcdc34851e67d69f593e8853d1
alish-gautam <alishgautam890@gmail.com>
2026-06-27 09:36:45 +0545

    while loop done

---
+++ JavascriptFundamentals/Loops/task3.js
@@ -0,0 +1,8 @@
+// Replace "for" with "while"
+// importance: 5
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+let i = 0;
+while (i < 3) {
+  console.log(`number ${i}!`);
+  i++;
+}
