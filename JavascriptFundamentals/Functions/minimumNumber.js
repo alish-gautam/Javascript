e205faa88e38104b7a40f8cbe1bbe54c358f939b
alish-gautam <alishgautam890@gmail.com>
2026-06-30 06:54:57 +0545

    Function to find the smallest number done

---
+++ JavascriptFundamentals/Functions/minimumNumber.js
@@ -0,0 +1,15 @@
+// Function min(a, b)
+// importance: 1
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// For instance:
+
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+console.log(min(3, -1));
